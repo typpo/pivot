37b5d69bdca35771b46e5d7bcd2df7927fe44488
Vadim Ogievetsky <vadimon@gmail.com>
2016-08-15 09:45:12 -0700

    get format -> getFormat

--- src/client/visualizations/table/table.tsx
+++ src/client/visualizations/table/table.tsx
@@ -234,7 +234,7 @@ export class Table extends BaseVisualization<TableState> {
     return measuresArray.map(measure => {
       var measureName = measure.name;
       var measureValues = flatData.map((d: Datum) => d[measureName] as number);
-      return formatterFromData(measureValues, measure.get('format'));
+      return formatterFromData(measureValues, measure.getFormat());
     });
   }
 
